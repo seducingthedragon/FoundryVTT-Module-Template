df8805aa53b71b8c64fa602a3aa40eae64b26c9a
theripper93 <tsplab@gmail.com>
2025-01-21 17:45:38 +0100

    a

--- scripts/lib/welcome.js
+++ scripts/lib/welcome.js
@@ -6,25 +6,26 @@ export function showWelcome() {
     if (!game.user.isGM) return;
     const module = game.modules.get(MODULE_ID);
     const WIKI_URL = module.protected ? `https://wiki.theripper93.com/premium/${MODULE_ID}` : `https://wiki.theripper93.com/free/${MODULE_ID}`;
-    const MESSAGE = `<h1>${module.name}</h1>
-    <p><strong>New to ${module.name}? Visit the <a href="${WIKI_URL}">Wiki<a> for a quickstart guide and resources.</strong></p>
-    <p>Special thanks to all my <a href="https://www.patreon.com/theripper93">Patreons</a> for making ongoing updates and development possible. Supporting gives you access to <strong>30+ premium modules</strong> and priority support.</p>
+    const MESSAGE = `<h1>${module.title}</h1>
+    <p>by <a href="https://theripper93.com">theripper93</a></p>
+    <p><strong>New to ${module.title}? Visit the <a href="${WIKI_URL}">Wiki</a> for a quickstart guide and resources.</strong></p>
+    <p>Special thanks to all my <a href="https://www.patreon.com/theripper93">Patreons</a> for making ongoing updates and development possible. Supporting gives you access to <strong>30+ premium modules</strong> and <strong>priority support</strong>.</p>
     <p><strong>Explore all resources and modules:</strong></p> <ul>
     <li><a href="https://theripper93.com/">Check out all my free and premium modules</a></li>
-    <li><a href="${WIKI_URL}">${module.name} Wiki</a></li>
+    <li><a href="${WIKI_URL}">${module.title} Wiki</a></li>
     <li><a href="https://www.patreon.com/theripper93">Support on Patreon</a></li>
     <li><a href="https://discord.theripper93.com/">Discord Channel</a></li>
     </ul> `;
 
 
-    game.settings.register(MODULE_SCOPE, SHOWN_KEY, {
+    game.settings.register(MODULE_ID, SHOWN_KEY, {
         default: false,
         type: Boolean,
         scope: "world",
         config: false,
     });
 
-    if (!game.settings.get(MODULE_SCOPE, SHOWN_KEY)) {
+    if (!game.settings.get(MODULE_ID, SHOWN_KEY)) {
 
         ChatMessage.create({
             user: game.user.id,
@@ -33,6 +34,6 @@ export function showWelcome() {
             content: MESSAGE,
         });
 
-        game.settings.set(MODULE_SCOPE, SHOWN_KEY, true);
+        game.settings.set(MODULE_ID, SHOWN_KEY, true);
     }
 }
